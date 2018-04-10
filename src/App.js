8eb5eb33b4432d059dad1836af2936334667d9ea
Eric Tang <erictang1729@gmail.com>
2018-04-09 21:04:05 -0700

    New Material-UI

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import TestButton from './components/TestButton'
-
 class App extends Component {
   render() {
     return (
@@ -15,10 +12,6 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-
-          <MuiThemeProvider>
-            <TestButton />
-          </MuiThemeProvider>
         </p>
       </div>
     );
