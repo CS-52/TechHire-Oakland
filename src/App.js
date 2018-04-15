a0dbbb1631c0d026eb3b51b0dbb0cb075ca00156
cpeale <charlotte.peale@gmail.com>
2018-04-15 16:31:17 -0700

    Created Survey with text field and multiple choice questions, that keeps track of inputs in real time and clears on submit.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Survey from './components/survey';
 
 class App extends Component {
+
+  state = {
+    fields: {}
+  };
+
+  onChange = updatedValue => {
+    this.setState({fields: {
+      ...this.state.fields,
+      ...updatedValue
+    }
+  });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Survey onChange={fields => this.onChange(fields)}/>
+        <p>{JSON.stringify(this.state.fields, null, 2)}</p>
       </div>
     );
   }
