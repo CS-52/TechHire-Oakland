4cb814c61058a9248887bb033006d6659fafe493
Eric Tang <erictang1729@gmail.com>
2018-04-20 21:04:16 -0700

    Actual merge of master.

--- src/App.js
+++ src/App.js
@@ -50,19 +50,7 @@ class App extends Component {
 
     return (
       <div className="App">
-<<<<<<< HEAD
         <TrainingPartnerLayout />
-=======
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
-        <div className = "body_container">
-          <p>This is a text</p>
-        </div>
-
->>>>>>> master
       </div>
     );
   }
