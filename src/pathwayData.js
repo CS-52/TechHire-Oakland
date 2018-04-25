588e31a3f8fe6ecb720a0000f9dd8c3fb586046c
Thapelo <Thapz.sebolai@gmail.com>
2018-04-24 18:45:18 -0700

    Fixed pathways.js issue

---
+++ src/pathwayData.js
@@ -0,0 +1,8 @@
+import schoolData from './schoolData'
+var pathway = {
+  beginner: [schoolData.schools[0], schoolData.schools[4]],
+  intermediate: [schoolData.schools[1]],
+  advanced: [schoolData.schools[3], schoolData.schools[2]]
+}
+
+export default pathway;
---
+++ src/schoolData.js
@@ -0,0 +1,61 @@
+
+var schoolData = {
+      schools: [
+        {
+      key: 1,
+      name: 'The Hidden Genius Project',
+          imgPath: '/images/hidden-genius-project.png',
+          info: {
+            targetSkills: ['Mobile Development', 'SQL'],
+        location: 'Berkeley',
+        website: 'hidden-genius-test.com',
+        contact: 'hidden-genius-contact-test@example.com'
+      }
+    },
+        {
+      key: 2,
+          name: 'Techtonica',
+          imgPath: '/images/techtonica.png',
+          info: {
+            targetSkills: ['Web Development'],
+        location: 'San Francisco',
+        website: 'techtonica-test.com',
+        contact: 'techtonica-contact-test@example.com'
+      }
+    },
+        {
+      key: 3,
+          name: 'Rithm School',
+          imgPath: '/images/rithm-school.jpg',
+          info: {
+            targetSkills: ['Web Development', 'Ruby'],
+        location: 'San Francisco',
+        website: 'rithm-test.com',
+        contact: 'rithm-contact-test@example.com'
+          }
+        },
+    {
+      key: 4,
+      name: 'Learners Guild',
+      imgPath: '/images/learners-guild.png',
+      info: {
+        targetSkills: ['Web Development', 'Data Science'],
+        location: 'Oakland',
+        website: 'learners-guild-test.com',
+        contact: 'lg-contact-test@example.com'
+      }
+    },
+    {
+      key: 5,
+          name: 'Merritt College',
+          imgPath: '/images/merritt-college.png',
+          info: {
+            targetSkills: ['Intro CS', 'Python'],
+        location: 'Oakland',
+        website: 'merritt-test.com',
+        contact: 'merritt-college-contact-test@example.com'
+      }
+    }
+      ]  
+    }
+export default schoolData
