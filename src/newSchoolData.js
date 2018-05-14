1219fe1b1a3fceebace6464bb8633acac5fcb7b6
cpeale <charlotte.peale@gmail.com>
2018-05-13 22:10:30 -0700

    Re-structured pathway visualization to match database structure, began firebase integration

---
+++ src/newSchoolData.js
@@ -0,0 +1,41 @@
+var schoolData = {
+      schools: {
+      'The Hidden Genius Project': {
+      imgPath: '/images/hidden_genius.png',
+      cost: '$0.00',
+      location: 'Berkeley',
+      website: 'hidden-genius-test.com',
+      contact: 'hidden-genius-contact-test@example.com'
+    },
+    'Techtonica': {
+        imgPath: '/images/techhire_training_partner_techtonica.png',
+        cost: '$14,000.00',
+        location: 'San Francisco',
+        website: 'techtonica-test.com',
+        contact: 'techtonica-contact-test@example.com'
+
+    },
+    'Rithm School': {
+        imgPath: '/images/rithm_school.png',
+        cost: '$2,192.00',
+        location: 'San Francisco',
+        website: 'rithm-test.com',
+        contact: 'rithm-contact-test@example.com' 
+        },
+    'Learners Guild': {
+      imgPath: '/images/learners-guild.png',
+      cost: '$16,000.00',
+      location: 'Oakland',
+      website: 'learners-guild-test.com',
+      contact: 'lg-contact-test@example.com'
+    },
+    'Merritt College': {
+        imgPath: '/images/merritt_college.png',
+        cost: '$2,000.00',
+        location: 'Oakland',
+        website: 'merritt-test.com',
+        contact: 'merritt-college-contact-test@example.com' 
+    }
+      }  
+    }
+export default schoolData
